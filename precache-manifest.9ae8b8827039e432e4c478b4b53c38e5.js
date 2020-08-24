1739e7f24573aea73b56dabbcd738b4048a2d465
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2020-08-24 23:14:13 +0200

    Updates

---
+++ precache-manifest.9ae8b8827039e432e4c478b4b53c38e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910ebd8d6db0983726899813bdc9ca9f",
+    "url": "/my-portfolio-project/index.html"
+  },
+  {
+    "revision": "60333ea5c5ac9f586bc9",
+    "url": "/my-portfolio-project/static/css/main.dd306189.chunk.css"
+  },
+  {
+    "revision": "62916f93003471286db3",
+    "url": "/my-portfolio-project/static/js/2.c3bea5a4.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/my-portfolio-project/static/js/2.c3bea5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60333ea5c5ac9f586bc9",
+    "url": "/my-portfolio-project/static/js/main.c0ad0d47.chunk.js"
+  },
+  {
+    "revision": "fa225a2e9d161dcde7e7",
+    "url": "/my-portfolio-project/static/js/runtime-main.ba56dd34.js"
+  }
+]);
